862a17dce4fd9eac60994fb834c2f64041c13344
Jack Polo <jackpolo@Jacks-MacBook-Pro.local>
2019-05-24 11:30:12 -0400

    completed hw

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,54 @@
-
+let timer;
+let counter = 5;
 let score = 0;
 let currentQuestion = 0;
 let questions = [
     {
-        title: "Which instrament do I favor?",
+        title: "Which instrument do I favor?",
         answer: ['Drums', 'Guitar', 'Piano', 'Sax'],
-        correct: 1
+        correct: 0
     },
 
     {
         title: "Whats my favorite type of music?",
         answer: ['Hip Hop', 'R&B', 'Alternative', 'Jazz'],
-        correct: 1
+        correct: 0
     },
 
     {
         title: "Who do you think I believe is the best rapper ever?",
         answer: ['Nas', 'Biggie', 'Jay-Z', 'Rakim'],
-        correct: 1
+        correct: 0
     },
 
     {
         title: "Which decade of Hip Hop do I favor?",
-        answer: ['90s', '80xs', 'New Millinium', '2019'],
-        correct: 1
+        answer: ['90s', '80xs', 'New Millennium', '2019'],
+        correct: 0
     },
 
     {
         title: "Which of these artist have I recorded with?",
         answer: ['Lloyd Banks', '50 Cent', 'Tony Yayo', 'Young Buck'],
-        correct: 1
+        correct: 0
     },
 
     {
         title: "Which is the correct birth date of Hip Hop?",
         answer: ['1973', '1975', '1978', '1980'],
-        correct: 1
+        correct: 0
     },
 
     {
         title: "Who started Def Jam?",
-        answer: ['Russel Simons', 'Jay-Z', 'Rick Rubin', 'Leor Cohen'],
-        correct: 1
+        answer: ['Russel Simmons', 'Jay-Z', 'Rick Rubin', 'Leor Cohen'],
+        correct: 0
     },
 
     {
         title: "Which is the most legendary Hip Hop studio of all time?",
         answer: ['Power Play Studios', 'D&D Studios', 'Quad Studios', 'Battery Studios'],
-        correct: 1
+        correct: 0
     },
 ];
 
@@ -67,7 +68,7 @@ $(document).ready(function () {
         $(this).addClass('selected');
     });
 
-    $('.quiz a').click(function (e) {
+    $('.submit-answer').click(function (e) {
         e.preventDefault();
         if ($('li.selected').length) {
             let guess = parseInt($('li.selected').attr('id'));
@@ -89,33 +90,47 @@ function showQuestion() {
     let question = questions[currentQuestion];
     $('.quiz h2').text(question.title);
     $('.quiz ul').html('');
-    for (var i = 0; i < question.answwers.length; i++) {
-        $('.quiz ul').append("<li id='" + i + "'>" + question.answwers[i] + "</li >");
+    for (var i = 0; i < question.answer.length; i++) {
+        $('.quiz ul').append("<li class='answer' id='" + i + "'>" + question.answer[i] + "</li >");
     }
-
-
-    function checkAnswer() {
-        let question = questions[currentQuestion];
-        if (question.correct === guess) {
-            score++;
-        }
-        currentQuestion++;
-        if (currentQuestion >= questions.length) {
-            showSummary();
-        } else {
-            showQuestion();
-        }
+    timer = setInterval(countDown, 1000);
+}
+function countDown() {
+    counter--
+    $(".counter").text(counter)
+    if (counter === 0) {
+        clearInterval(timer)
+        counter = 5
+        currentQuestion++
+        showQuestion()
     }
-    function showSummary() {
-        $('.quiz').hide();
-        $('.summary').show();
-        $('summary p').text("Congrats you scored " + scored + " out of " + questions.length + " correct!")
-    }
+}
 
-    function restart.quiz() {
-        $('.summary').hide();
-        $('.quiz').show();
-        score = 0;
-        currentQuestion = 0;
+function checkAnswer(guess) {
+    let question = questions[currentQuestion];
+    if (question.correct === guess) {
+        score++;
+    }
+    currentQuestion++;
+    clearInterval(timer)
+        counter = 5
+    if (currentQuestion >= questions.length) {
+        showSummary();
+    } else {
         showQuestion();
     }
+}
+
+function showSummary() {
+    $('.quiz').hide();
+    $('.summary').show();
+    $('.summary-text').text("Congrats you scored " + score + " out of " + questions.length + " correct!")
+}
+
+function restartQuiz() {
+    $('.summary').hide();
+    $('.quiz').show();
+    score = 0;
+    currentQuestion = 0;
+    showQuestion();
+}
